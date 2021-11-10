44d69ff91cc53b0cdb7eb857eb35bd93acfb7d66
irshadi <irshadi@delman.io>
2021-11-10 13:37:32 +0700

    feat: add cypress

---
+++ cypress/integration/Homepage.js
@@ -0,0 +1,6 @@
+describe("Homepage Test", () => {
+  it("Should be able to render homepage");
+  it("Should be able to search movies");
+  it("Should be able to scroll through movies");
+  it("Should be able to show error when search result is not found");
+});
